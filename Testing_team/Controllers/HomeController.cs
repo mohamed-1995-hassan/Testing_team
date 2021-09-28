ed8c8a09c67301ebbc03cb9d09588fd575d05ced
mohamed-1995-hassan <59643006+mohamed-1995-hassan@users.noreply.github.com>
2021-09-28 08:01:07 +0200

    myFinb

--- Testing_team/Controllers/HomeController.cs
+++ Testing_team/Controllers/HomeController.cs
@@ -25,13 +25,8 @@ namespace Testing_team.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            int x = 7;
+            return View(x);
         }
     }
 }
