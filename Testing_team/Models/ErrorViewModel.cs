85a8800fd7afcbf6398c851d9aa7fbeee5aa2e1a
mohamed-1995-hassan <59643006+mohamed-1995-hassan@users.noreply.github.com>
2021-09-28 07:57:44 +0200

    Add project files.

---
+++ Testing_team/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Testing_team.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
